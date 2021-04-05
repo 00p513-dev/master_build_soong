PixelExperience/build_soong

soong: Drop custom soong product variables
 * This reverts commits c59e356, ce8c7b4, f6807aa in preparation
   for using Soong config variables as per Google's docs.

Change-Id: Ice01dcd8da10e3e2c38394acd9fe9a9ccb1d9784

scripts/microfactory.bash
@@ -59,7 +59,7 @@ function soong_build_go
     BUILDDIR=$(getoutdir) \
       SRCDIR=${TOP} \
       BLUEPRINTDIR=${TOP}/build/blueprint \
-      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong -pkg-path aosp/soong=${TOP}/vendor/aosp/build/soong -pkg-path github.com/golang/protobuf=${TOP}/external/golang-protobuf" \
+      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong -pkg-path github.com/golang/protobuf=${TOP}/external/golang-protobuf" \
       build_go $@
 }
 

00p513-dev/master_build_soong

scripts/microfactory.bash
@@ -59,7 +59,7 @@ function soong_build_go
     BUILDDIR=$(getoutdir) \
       SRCDIR=${TOP} \
       BLUEPRINTDIR=${TOP}/build/blueprint \
-      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong -pkg-path  stormbreaker/soong=${TOP}/vendor/stormbreaker/build/soong -pkg-path github.com/golang/protobuf=${TOP}/external/golang-protobuf" \
+      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong -pkg-path gitthub.com/golang/protobuf=${TOP}/external/golang-protobuf" \
       build_go $@
 }
 